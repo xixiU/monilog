40019b3d21c0c8b2bd35a6953632b497517f9e02
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-07-18 17:06:24 +0800

    fx

--- mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatislogspringbootstarter/MybatisSqlFilterAutoConfiguration.java
+++ mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatislogspringbootstarter/MybatisSqlFilterAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties
 @ConditionalOnProperty(prefix = "monitor.log.mybatis", name = "enable", havingValue = "true", matchIfMissing = true)
 @Configuration
 public class MybatisSqlFilterAutoConfiguration {
