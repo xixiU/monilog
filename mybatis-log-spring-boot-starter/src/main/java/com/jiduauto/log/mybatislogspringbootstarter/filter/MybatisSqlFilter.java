d7b6043b73ef04df5bdeb5eebecbba11ff66a787
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-07-18 16:42:35 +0800

    fx provided

--- mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatislogspringbootstarter/filter/MybatisSqlFilter.java
+++ mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatislogspringbootstarter/filter/MybatisSqlFilter.java
@@ -5,14 +5,12 @@ import com.jiduauto.log.enums.LogPoint;
 import com.jiduauto.log.model.MonitorLogParams;
 import com.jiduauto.log.util.MonitorUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Statement;
