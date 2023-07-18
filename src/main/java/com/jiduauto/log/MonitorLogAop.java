e42d8842a31877726362eca153f99361f5b9d370
pei.ye01 <pei.ye01@jiduauto.com>
2023-07-18 15:17:51 +0800

    增加aop对mapper及mq的支持

--- src/main/java/com/jiduauto/log/MonitorLogAop.java
+++ src/main/java/com/jiduauto/log/MonitorLogAop.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-
-import java.util.List;
-
 /**
  * @author yp
  */
@@ -15,72 +12,11 @@ import java.util.List;
 class MonitorLogAop {
     private final MonitorLogPrinter logPrinter;
 
-    private final MonitorLogProperties properties;
-
     public MonitorLogAop(MonitorLogPrinter logPrinter, MonitorLogProperties properties) {
         this.logPrinter = logPrinter;
-        this.properties = properties;
     }
 
-    private String dao() {
-        return join(properties.getDaoAopExpressions());
-    }
-
-    private String rpc() {
-        return join(properties.getRpcAopExpressions());
-    }
-
-    private String mqConsumer() {
-        return join(properties.getMqConsumerAopExpressions());
-    }
-
-    private String mqProducer() {
-        return join(properties.getMqProducerAopExpressions());
-    }
-
-    private String http() {
-        return join(properties.getHttpAopExpressions());
-    }
-
-    private String job() {
-        return join(properties.getJobAopExpressions());
-    }
-
-    private String custom() {
-        return "@within(com.jiduauto.log.MonitorLog) ||@annotation(com.jiduauto.log.MonitorLog) && execution(public * *(..))";
-    }
-
-    @Around("execution(String http())")
-    public Object doHttp(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.WEB_ENTRY);
-    }
-
-    @Around("execution(String rpc())")
-    public Object doRpc(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.REMOTE_CLIENT);
-    }
-
-    @Around("execution(String mqConsumer())")
-    public Object doMqConsumer(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.MSG_ENTRY);
-    }
-
-    @Around("execution(String mqProducer())")
-    public Object doMqProducer(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.MSG_PRODUCER);
-    }
-
-    @Around("execution(String dao())")
-    public Object doDao(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.DAL_CLIENT);
-    }
-
-    @Around("execution(String job())")
-    public Object doJob(ProceedingJoinPoint pjp) throws Throwable {
-        return processAround(pjp, LogPoint.TASK_ENTRY);
-    }
-
-    @Around("execution(String custom())")
+    @Around("@within(com.jiduauto.log.MonitorLog) || @annotation(com.jiduauto.log.MonitorLog) && execution(public * *(..))")
     public Object doCustom(ProceedingJoinPoint pjp) throws Throwable {
         return processAround(pjp, null);
     }
@@ -151,10 +87,5 @@ class MonitorLogAop {
 
     private void beforeReturn(MonitorLogAspectCtx ctx) {
         //...
-    }
-
-
-    private static String join(List<String> patterns) {
-        return patterns == null || patterns.isEmpty() ? "" : String.join("||", patterns);
     }
 }
