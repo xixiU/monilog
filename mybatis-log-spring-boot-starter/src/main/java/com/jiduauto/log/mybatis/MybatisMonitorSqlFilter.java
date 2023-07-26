6b9e1307ffd980a6db882ec077c81c9c4f9e955b
pei.ye01 <pei.ye01@jiduauto.com>
2023-07-26 15:25:06 +0800

    Merge branch 'feature/monitor' of jidudev.com:vehicle/sale-support/monitor-log-spring-boot-starter into feature/monitor
    # Conflicts:
    #	core-log-spring-boot-starter/src/main/java/com/jiduauto/log/core/service/DefaultMonitorLogPrinter.java

--- mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatis/MybatisMonitorSqlFilter.java
+++ mybatis-log-spring-boot-starter/src/main/java/com/jiduauto/log/mybatis/MybatisMonitorSqlFilter.java
@@ -10,12 +10,14 @@ import com.jiduauto.log.core.util.MonitorLogUtil;
 import com.metric.MetricMonitor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
@@ -23,7 +25,8 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.lang.reflect.Proxy;
 import java.sql.Statement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ：xiaoxu.bao
@@ -31,8 +34,7 @@ import java.util.*;
  */
 @Intercepts({
         @Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
-        @Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
-
+        @Signature(type = StatementHandler.class, method = "update", args = {Statement.class})
 })
 @Slf4j
 class MybatisMonitorSqlFilter implements Interceptor {
@@ -41,7 +43,6 @@ class MybatisMonitorSqlFilter implements Interceptor {
      * sql耗时过长
      */
     private static final String SQL = "sql";
-
     private static final String SQL_COST_TOO_LONG = "sqlCostTooLang";
     /**
      * 超时时间，单位毫秒，默认2000毫秒
@@ -49,46 +50,37 @@ class MybatisMonitorSqlFilter implements Interceptor {
     @Value("${monitor.log.mybatis.long.query.time:2000}")
     private Long longQueryTime;
 
-    private static Set<Class<?>> MAPPER_CLASS_SET = null;
-
-    private static Set<String> FULL_MAPPER_IDS = null;
-
     @SneakyThrows
     @Override
     public Object intercept(Invocation invocation) {
         long nowTime = System.currentTimeMillis();
 
         MonitorLogParams logParams = new MonitorLogParams();
-        // 获取调用的目标对象
-        Object expectedStatementHandler = getStatementHandlerObject(invocation);
-        if (expectedStatementHandler == null) {
-            invocation.proceed();
-            return null;
-        }
-        StatementHandler statementHandler = (StatementHandler) expectedStatementHandler;
-        MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
-
-        MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
-        if (CollectionUtils.isEmpty(FULL_MAPPER_IDS) || CollectionUtils.isEmpty(MAPPER_CLASS_SET)) {
-            buildMapperInfos(mappedStatement);
-        }
-        String mapperId = mappedStatement.getId();
-        Class<?> serviceCls = Class.forName(mapperId.substring(0, mapperId.lastIndexOf('.')));
-        String methodName = mapperId.substring(mapperId.lastIndexOf('.') + 1);
-        logParams.setLogPoint(LogPoint.DAL_CLIENT);
-        logParams.setService(serviceCls.getSimpleName());
-        logParams.setServiceCls(serviceCls);
-        logParams.setAction(methodName);
-        logParams.setMsgCode(ErrorEnum.SUCCESS.name());
-        logParams.setMsgInfo(ErrorEnum.SUCCESS.getMsg());
-
-        //TODO 取logParser
         List<String> tags = new ArrayList<>();
         long costTime = 0;
         try {
+            // 获取调用的目标对象
+            Object expectedStatementHandler = getStatementHandlerObject(invocation);
+            if (expectedStatementHandler == null) {
+                invocation.proceed();
+                return null;
+            }
+            StatementHandler statementHandler = (StatementHandler) expectedStatementHandler;
+            MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+            MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+            String mapperId = mappedStatement.getId();
+            Class<?> serviceCls = Class.forName(mapperId.substring(0, mapperId.lastIndexOf('.')));
+            String methodName = mapperId.substring(mapperId.lastIndexOf('.') + 1);
+
+            logParams.setLogPoint(LogPoint.DAL_CLIENT);
+            logParams.setService(serviceCls.getSimpleName());
+            logParams.setServiceCls(serviceCls);
+            logParams.setAction(methodName);
+            logParams.setMsgCode(ErrorEnum.SUCCESS.name());
+            logParams.setMsgInfo(ErrorEnum.SUCCESS.getMsg());
             Object obj = invocation.proceed();
             BoundSql boundSql = statementHandler.getBoundSql();
-            String sql = boundSql.getSql();
+            String sql = boundSql.getSql().replace("\n|\r|\\s+", " ");
             logParams.setInput(new String[]{sql});
             logParams.setOutput(obj);
             costTime = System.currentTimeMillis() - nowTime;
@@ -118,35 +110,6 @@ class MybatisMonitorSqlFilter implements Interceptor {
             MonitorLogUtil.log(logParams);
         }
     }
-
-    private void buildMapperInfos(MappedStatement mappedStatement) {
-        //临时调用用
-        Collection<Class<?>> mappers = mappedStatement.getConfiguration().getMapperRegistry().getMappers();
-        MAPPER_CLASS_SET = new HashSet<>(mappers);
-
-        Collection<?> mappedStatements = mappedStatement.getConfiguration().getMappedStatements();
-        try {
-            Set<String> set = new HashSet<>();
-            for (Object ms : mappedStatements) {
-                if (ms instanceof MappedStatement) {
-                    set.add(((MappedStatement) ms).getId());
-                }
-            }
-            FULL_MAPPER_IDS = set;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Object plugin(Object target) {
-        return Plugin.wrap(target, this);
-    }
-
-    @Override
-    public void setProperties(Properties properties) {
-    }
-
     private Object getStatementHandlerObject(Invocation invocation) {
         Object expectedStatementHandler = invocation.getTarget();
         while (Proxy.isProxyClass(expectedStatementHandler.getClass())) {
