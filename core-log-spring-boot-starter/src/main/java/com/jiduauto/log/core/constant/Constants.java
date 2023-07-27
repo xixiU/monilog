7b9499f5e95e0a25d1a2f45502fb934339c860e9
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-07-27 14:09:46 +0800

    remove unused code

--- core-log-spring-boot-starter/src/main/java/com/jiduauto/log/core/constant/Constants.java
+++ core-log-spring-boot-starter/src/main/java/com/jiduauto/log/core/constant/Constants.java
@@ -4,24 +4,15 @@ package com.jiduauto.log.core.constant;
  * @author : udon 2022-04-21
  */
 public class Constants {
-
-    public static final String TRACE_ID = "trace_id";
-
     /**
      * 业务监控tag值为空的处理
      */
     public static final String NO_VALUE_CODE = "00";
 
-
-    public static final String COMMA_BRACKETS = ",}";
-
-    public static final String BRACKETS = "}";
-
     /**
      * 下划线
      */
     public static final String UNDERLINE = "_";
-
 
     /**
      * 点
@@ -80,14 +71,5 @@ public class Constants {
      * 环境
      */
     public static final String ENV = "env";
-
-
-    // mq相关常量
-
-    /**
-     * rocketmq header name
-     */
-    public static final String TX_ID_HEADER_NAME = "rocketmq_TRANSACTION_ID";
-    public static final String MSG_ID_HEADER_NAME = "id";
 
 }
