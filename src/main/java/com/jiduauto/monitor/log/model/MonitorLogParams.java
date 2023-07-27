69ff7b6fd08a10f3bdee5eee02a56905f131a551
pei.ye01 <pei.ye01@jiduauto.com>
2023-07-28 01:46:45 +0800

    Merge branch 'feature/monitor' of jidudev.com:vehicle/sale-support/monitor-log-spring-boot-starter into feature/monitor
    # Conflicts:
    #	src/main/java/com/jiduauto/monitor/log/RequestWrapper.java
    #	src/main/java/com/jiduauto/monitor/log/WebMonitorLogConfiguration.java

--- src/main/java/com/jiduauto/monitor/log/model/MonitorLogParams.java
+++ src/main/java/com/jiduauto/monitor/log/model/MonitorLogParams.java
@@ -1,8 +1,11 @@
 package com.jiduauto.monitor.log.model;
 
+import com.jiduauto.monitor.log.enums.ErrorEnum;
 import com.jiduauto.monitor.log.enums.LogPoint;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author yp
  * @date 2023/07/12
@@ -22,4 +25,12 @@ public class MonitorLogParams {
     private Throwable exception;
     private Object[] input;
     private Object output;
+
+    public String getMsgCode() {
+        return StringUtils.isBlank(msgCode) ? (success ? ErrorEnum.SUCCESS.name() : ErrorEnum.FAILED.name()) : msgCode;
+    }
+
+    public String getMsgInfo() {
+        return StringUtils.isBlank(msgInfo) ? (success ? ErrorEnum.SUCCESS.getMsg() : ErrorEnum.FAILED.getMsg()) : msgInfo;
+    }
 }
