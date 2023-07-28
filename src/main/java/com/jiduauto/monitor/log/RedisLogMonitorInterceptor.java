aee8d6d42cab8b49e54d21ee27a80c9a56ea1e8e
pei.ye01 <pei.ye01@jiduauto.com>
2023-07-28 23:36:40 +0800

    Merge branch 'feature/monitor' of jidudev.com:vehicle/sale-support/monitor-log-spring-boot-starter into feature/monitor
    # Conflicts:
    #	src/main/java/com/jiduauto/monitor/log/MonitorLogAutoConfiguration.java

--- src/main/java/com/jiduauto/monitor/log/RedisLogMonitorInterceptor.java
+++ src/main/java/com/jiduauto/monitor/log/RedisLogMonitorInterceptor.java
@@ -1,72 +1,24 @@
 package com.jiduauto.monitor.log;
 
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.core.Ordered;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
 class RedisLogMonitorInterceptor {
-    static class RedisTemplateEnhanceProcessor implements BeanPostProcessor, Ordered {
-        private final MonitorLogProperties.RedisProperties redisProperties;
 
-        public RedisTemplateEnhanceProcessor(MonitorLogProperties.RedisProperties redisProperties) {
-            this.redisProperties = redisProperties;
-        }
-
-        @Override
-        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-            if (bean instanceof RedisTemplate && !(bean instanceof RedisLogMonitorInterceptor.EnhancedRedisTemplate)) {
-                if (bean instanceof StringRedisTemplate) {
-                    return new RedisLogMonitorInterceptor.EnhancedStringRedisTemplate((StringRedisTemplate) bean, redisProperties);
-                } else {
-                    return new RedisLogMonitorInterceptor.EnhancedRedisTemplate<>((RedisTemplate<?, ?>) bean, redisProperties);
-                }
-            }
-            return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
-        }
-
-        @Override
-        public int getOrder() {
-            return Integer.MAX_VALUE;
-        }
-    }
-
-    @AllArgsConstructor
-    private static class EnhancedStringRedisTemplate extends StringRedisTemplate implements RedisExecutor {
-        private final StringRedisTemplate redisTemplate;
-        private final MonitorLogProperties.RedisProperties redisProperties;
-
-        @Override
-        public <T> T execute(RedisCallback<T> action, boolean exposeConnection, boolean pipeline) {
-            return doExecute(redisTemplate, action, exposeConnection, pipeline);
-        }
-    }
-
-    @AllArgsConstructor
-    private static class EnhancedRedisTemplate<K, V> extends RedisTemplate<K, V> implements RedisExecutor {
-        private final RedisTemplate<K, V> redisTemplate;
-        private final MonitorLogProperties.RedisProperties redisProperties;
-
-        @Override
-        public <T> T execute(RedisCallback<T> action, boolean exposeConnection, boolean pipeline) {
-            return doExecute(redisTemplate, action, exposeConnection, pipeline);
-        }
-    }
-
-    interface RedisExecutor {
-        default <T> T doExecute(RedisTemplate redisTemplate, RedisCallback<T> action, boolean exposeConnection, boolean pipeline) {
+    @Aspect
+    static class RedisTemplateEnhancer {
+        @Around("execution(public * *.execute(org.springframework.data.redis.core.RedisCallback, boolean ,boolean))")
+        Object intercept(ProceedingJoinPoint pjp) {
             long start = System.currentTimeMillis();
             long cost = 0;
             Throwable ex = null;
-            T ret = null;
+            Object ret = null;
             try {
-                ret = (T) redisTemplate.execute(action, exposeConnection, pipeline);
+                ret = pjp.proceed();
             } catch (Throwable e) {
                 ex = e;
             } finally {
