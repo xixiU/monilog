6a686451aca118ed384b93469d70412545fc7578
pei.ye01 <pei.ye01@jiduauto.com>
2023-07-31 16:24:20 +0800

    更改

--- src/main/java/com/jiduauto/monitor/log/XxlJobLogMonitorInterceptor.java
+++ src/main/java/com/jiduauto/monitor/log/XxlJobLogMonitorInterceptor.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 class XxlJobLogMonitorInterceptor {
-    @Around("execution(public * com.xxl.job.core.handler.IJobHandler+.*(..)) || @annotation(com.xxl.job.core.handler.annotation.XxlJob)")
+    @Around("execution(public * com.xxl.job.core.handler.IJobHandler+.execute(..)) || @annotation(com.xxl.job.core.handler.annotation.XxlJob)")
     public Object interceptXxlJob(ProceedingJoinPoint pjp) throws Throwable {
         String boolExpr = "$.code==" + ReturnT.SUCCESS_CODE;
         LogParser logParser = LogParser.Default.buildInstance(boolExpr);
