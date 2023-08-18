6bac1770d4ae8ace50b25491a796f68d509f20cd
pei.ye01 <pei.ye01@jiduauto.com>
2023-08-18 15:45:42 +0800

    优化idea告警

--- src/main/java/com/jiduauto/monilog/DefaultMoniLogPrinter.java
+++ src/main/java/com/jiduauto/monilog/DefaultMoniLogPrinter.java
@@ -46,27 +46,25 @@ class DefaultMoniLogPrinter implements MoniLogPrinter {
     }
 
     @Override
-    public void logLargeSize(MoniLogParams p, String key) {
+    public void logDigest(MoniLogParams p) {
         if (p == null) {
             return;
         }
-        if (p.getLogPoint() != LogPoint.redis || p.getOutput() == null || StringUtils.isBlank(key)) {
-            return;
-        }
-        MoniLogProperties.RedisProperties redisConf = moniLogProperties.getRedis();
-        if (redisConf == null || !redisConf.isEnable() || redisConf.getWarnForValueLength() <= 0) {
-            return;
-        }
-        long valueLen = 0;
-        try {
-            valueLen = RamUsageEstimator.sizeOf(p.getOutput());
-        } catch (Exception e) {
-            MoniLogUtil.innerDebug("parseResultSize error", e);
-        }
         Logger logger = getLogger(p);
-        if (valueLen > 0 && valueLen > redisConf.getWarnForValueLength() * ONE_KB) {
-            logger.error("{}size_too_large[{}]-{}.{}[key={}], size: {}", getLogPrefix(), p.getLogPoint(), p.getService(), p.getAction(), key, RamUsageEstimator.humanReadableUnits(valueLen));
+        String logPoint = p.getLogPoint().name();
+        String service = p.getService();
+        String action = p.getAction();
+        String success = p.isSuccess() ? "true" : "false";
+        String code = p.getMsgCode();
+        String msg = p.getMsgInfo();
+        String[] tags = p.getTags();
+        String tagStr = tags == null || tags.length == 0 ? "" : "|" + Arrays.toString(tags);
+        String rt = p.getCost() + "ms";
+        if (p.getException() != null) {
+            logger.error("{}digest_log[{}]-{}.{}|{}|{}|{}|{}{}", getLogPrefix(), logPoint, service, action, success, code, msg, rt, tagStr);
+            return;
         }
+        logger.info("{}digest_log[{}]-{}.{}|{}|{}|{}|{}{}", getLogPrefix(), logPoint, service, action, success, code, msg, rt, tagStr);
     }
 
     @Override
@@ -88,25 +86,24 @@ class DefaultMoniLogPrinter implements MoniLogPrinter {
     }
 
     @Override
-    public void logDigest(MoniLogParams p) {
-        if (p == null) {
+    public void logLargeSize(MoniLogParams p, String key) {
+        if (p == null || p.getLogPoint() != LogPoint.redis || p.getOutput() == null || StringUtils.isBlank(key)) {
             return;
+        }
+        MoniLogProperties.RedisProperties redisConf = moniLogProperties.getRedis();
+        if (redisConf == null || !redisConf.isEnable() || redisConf.getWarnForValueLength() <= 0) {
+            return;
+        }
+        long valueLen = 0;
+        try {
+            valueLen = RamUsageEstimator.sizeOf(p.getOutput());
+        } catch (Exception e) {
+            MoniLogUtil.innerDebug("parseResultSize error", e);
         }
         Logger logger = getLogger(p);
-        String logPoint = p.getLogPoint().name();
-        String service = p.getService();
-        String action = p.getAction();
-        String success = p.isSuccess() ? "true" : "false";
-        String code = p.getMsgCode();
-        String msg = p.getMsgInfo();
-        String[] tags = p.getTags();
-        String tagStr = tags == null || tags.length == 0 ? "" : "|" + Arrays.toString(tags);
-        String rt = p.getCost() + "ms";
-        if (p.getException() != null) {
-            logger.error("{}digest_log[{}]-{}.{}|{}|{}|{}|{}{}", getLogPrefix(), logPoint, service, action, success, code, msg, rt, tagStr);
-            return;
+        if (valueLen > 0 && valueLen > redisConf.getWarnForValueLength() * ONE_KB) {
+            logger.error("{}size_too_large[{}]-{}.{}[key={}], size: {}", getLogPrefix(), p.getLogPoint(), p.getService(), p.getAction(), key, RamUsageEstimator.humanReadableUnits(valueLen));
         }
-        logger.info("{}digest_log[{}]-{}.{}|{}|{}|{}|{}{}", getLogPrefix(), logPoint, service, action, success, code, msg, rt, tagStr);
     }
 
     private String formatLongText(Object o) {
