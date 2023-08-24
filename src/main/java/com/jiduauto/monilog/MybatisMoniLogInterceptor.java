e3428423b6291116e591a2b0defed8838caeff8c
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-08-24 11:41:43 +0800

    fx mybatis Intercepts

--- src/main/java/com/jiduauto/monilog/MybatisMoniLogInterceptor.java
+++ src/main/java/com/jiduauto/monilog/MybatisMoniLogInterceptor.java
@@ -3,6 +3,8 @@ package com.jiduauto.monilog;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
@@ -13,24 +15,27 @@ import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
 import java.lang.reflect.Proxy;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
 
 class MybatisMoniLogInterceptor {
+    //    @Intercepts({
+//            @Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
+//            @Signature(type = StatementHandler.class, method = "update", args = {Statement.class})
+//    })
     @Intercepts({
-            @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class}),
-            @Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
-            @Signature(type = StatementHandler.class, method = "update", args = {Statement.class})
+            @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+            @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+            @Signature(type = Executor.class, method = "queryCursor", args = {MappedStatement.class, Object.class, RowBounds.class}),
+            @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
     })
     @Slf4j
     static class MybatisInterceptor implements Interceptor {
         private static final Map<String, Class<?>> CACHED_CLASS = new HashMap<>();
-
 
         @SneakyThrows
         @Override
@@ -76,7 +81,7 @@ class MybatisMoniLogInterceptor {
                     }
                     throw e;
                 } else {//组件异常
-                    MoniLogUtil.innerDebug( "mybatisInterceptor process error", e);
+                    MoniLogUtil.innerDebug("mybatisInterceptor process error", e);
                     return obj;
                 }
             } finally {
@@ -90,36 +95,63 @@ class MybatisMoniLogInterceptor {
             String methodName;
 
             String sql;
+
+            MybatisInvocationInfo(Class<?> serviceCls, String methodName) {
+                this.serviceCls = serviceCls;
+                this.methodName = methodName;
+            }
         }
 
+        /**
+         * 此方法可以拦截StatementHandler与Executor接口；
+         * 在Executor接口中，当前拦截的方法第一个参数均是MappedStatement.class类型的对象,第二个参数均是Object.class类型的parameter参数
+         *
+         * @param invocation
+         * @return
+         */
         private static MybatisInvocationInfo parseMybatisExecuteInfo(Invocation invocation) {
             Class<?> serviceCls = invocation.getTarget().getClass();
             String methodName = invocation.getMethod().getName();
+            MybatisInvocationInfo info = new MybatisInvocationInfo(serviceCls, methodName);
             String sql = "[parseSqlFailed]";
             try {
-                Object expectedStatementHandler = getStatementHandlerObject(invocation);
-                if (expectedStatementHandler != null) {
-                    StatementHandler statementHandler = (StatementHandler) expectedStatementHandler;
-                    MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
-                    MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
-                    String mapperId = mappedStatement.getId();
-                    serviceCls = loadCls(mapperId.substring(0, mapperId.lastIndexOf('.')));
-                    methodName = mapperId.substring(mapperId.lastIndexOf('.') + 1);
-                    BoundSql boundSql = statementHandler.getBoundSql();
-                    //去除sql中的注释、换行、多余空格等
-                    sql = boundSql.getSql().replaceAll("--[^\n|\\\\n].+(\n|\\\\n)","").replaceAll("(\\\\n)+|\n+|\r+|\\s+"," ");
+                if (Executor.class.isAssignableFrom(serviceCls)) {
+                    Object[] args = invocation.getArgs();
+                    if (args == null || args.length < 2 || !MappedStatement.class.isAssignableFrom(args[0].getClass())) {
+                        return info;
+                    }
+                    MappedStatement mappedStatement = (MappedStatement) args[0];
+                    setServiceClsAndMethodName(mappedStatement, info);
+                    // 获取sql
+                    sql = mappedStatement.getBoundSql(args[1]).getSql().replaceAll("--[^\n|\\\\n].+(\n|\\\\n)", "").replaceAll("(\\\\n)+|\n+|\r+|\\s+", " ");
+                }
+                // 这段代码走不到了
+                if (StatementHandler.class.isAssignableFrom(serviceCls)) {
+                    Object expectedStatementHandler = getStatementHandlerObject(invocation);
+                    if (expectedStatementHandler != null) {
+                        StatementHandler statementHandler = (StatementHandler) expectedStatementHandler;
+                        MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+                        MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+                        setServiceClsAndMethodName(mappedStatement, info);
+                        BoundSql boundSql = statementHandler.getBoundSql();
+                        //去除sql中的注释、换行、多余空格等
+                        sql = boundSql.getSql().replaceAll("--[^\n|\\\\n].+(\n|\\\\n)", "").replaceAll("(\\\\n)+|\n+|\r+|\\s+", " ");
+                    }
                 }
             } catch (Throwable e) {
-                MoniLogUtil.innerDebug( "parseMybatisExecuteInfo error", e);
+                MoniLogUtil.innerDebug("parseMybatisExecuteInfo error", e);
             }
-            MybatisInvocationInfo info = new MybatisInvocationInfo();
-            info.serviceCls = serviceCls;
-            info.methodName = methodName;
             info.sql = sql;
             return info;
         }
 
-        private static Class<?> loadCls(String className){
+        public static void setServiceClsAndMethodName(MappedStatement mappedStatement, MybatisInvocationInfo info) {
+            String mapperId = mappedStatement.getId();
+            info.serviceCls = loadCls(mapperId.substring(0, mapperId.lastIndexOf('.')));
+            info.methodName = mapperId.substring(mapperId.lastIndexOf('.') + 1);
+        }
+
+        private static Class<?> loadCls(String className) {
             Class<?> aClass = CACHED_CLASS.get(className);
             if (aClass != null) {
                 return aClass;
