335f367c0232e507e49678abce1df7e7f42dcf9c
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-08-27 22:07:08 +0800

    rename monilogtag annotation

--- src/main/java/com/jiduauto/monilog/StringUtil.java
+++ src/main/java/com/jiduauto/monilog/StringUtil.java
@@ -58,11 +58,11 @@ class StringUtil {
      * 从注解中获取tag列表
      */
     public static List<String> getTagList(MoniLogTags logTags) {
-        if (logTags == null || logTags.tags() == null || logTags.tags().length == 0) {
+        if (logTags == null || logTags.value() == null || logTags.value().length == 0) {
             return new ArrayList<>();
         }
-        if (logTags.tags().length % 2 == 0) {
-            return Arrays.stream(logTags.tags()).map(String::trim).collect(Collectors.toList());
+        if (logTags.value().length % 2 == 0) {
+            return Arrays.stream(logTags.value()).map(String::trim).collect(Collectors.toList());
         } else {
             // prometheus tag是key,value结构，非偶数tag prometheus上报会报错，这里只打一行日志提醒
             log.error("tags length must be double，tags：{}", JSON.toJSONString(logTags));
