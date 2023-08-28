fe718cbf0626566635c7cdfec498eb9b37a33db8
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-08-28 10:06:39 +0800

    add common

--- src/main/java/com/jiduauto/monilog/MoniLogTags.java
+++ src/main/java/com/jiduauto/monilog/MoniLogTags.java
@@ -7,6 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * tag标记
+ * 配合@MoniLog注解使用，在web接口此注解单独使用即可生效。
+ * 详见：https://wiki.jiduauto.com/pages/viewpage.action?pageId=674041052
  * @author rongjie.yuan
  * @date 2023/7/25 11:37
  */
