0319e35b17392d68a80724d9d83c97de1a25c047
pei.ye01 <pei.ye01@jiduauto.com>
2023-08-30 19:54:10 +0800

    增加支持HttpAsyncClient

--- src/main/java/com/jiduauto/monilog/MoniLogSpringRunListener.java
+++ src/main/java/com/jiduauto/monilog/MoniLogSpringRunListener.java
@@ -8,15 +8,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.core.Ordered;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.jiduauto.monilog.MoniLogPostProcessor.HTTP_ASYNC_CLIENT_BUILDER;
+import static com.jiduauto.monilog.MoniLogPostProcessor.HTTP_CLIENT_BUILDER;
+
 /**
  * @author yp
  * @date 2023/08/08
  */
 @Slf4j
 final class MoniLogSpringRunListener implements SpringApplicationRunListener, Ordered {
+    private static final Map<String, AtomicBoolean> map = new HashMap<String, AtomicBoolean>() {{
+        put(HTTP_CLIENT_BUILDER, new AtomicBoolean());
+        put(HTTP_ASYNC_CLIENT_BUILDER, new AtomicBoolean());
+    }};
+
     private MoniLogSpringRunListener(SpringApplication app, String[] args) {
-        doEnhance(MoniLogPostProcessor.HTTP_CLIENT_BUILDER, "addInterceptorsForBuilder");
-        doEnhance(MoniLogPostProcessor.HTTP_ASYNC_CLIENT_BUILDER, "addInterceptorsForAsyncBuilder");
+        doEnhance(HTTP_CLIENT_BUILDER, "addInterceptorsForBuilder");
+        doEnhance(HTTP_ASYNC_CLIENT_BUILDER, "addInterceptorsForAsyncBuilder");
         SpringApplicationRunListener.super.starting();
     }
 
@@ -37,12 +49,16 @@ final class MoniLogSpringRunListener implements SpringApplicationRunListener, Or
     }
 
     private static void enhanceDefaultConstructor(String clsName, String descriptor, String srcCode) throws Throwable {
+        if (map.get(clsName).get()) {
+            return;
+        }
         ClassPool classPool = ClassPool.getDefault();
         CtClass ctCls = classPool.getCtClass(clsName);
         ctCls.getConstructor(descriptor).setBody(srcCode);
         ctCls.writeFile();
         Class<?> targetCls = ctCls.toClass();
         log.info("constructor of '{}' has bean enhanced...", targetCls.getCanonicalName());
+        map.get(clsName).set(true);
     }
 
     private static void enhanceMethod(String clsName, String method, String newMethodBody) throws Throwable {
