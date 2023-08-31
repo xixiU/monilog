db52a00aa5749726dc99a72ff0d06e7b69d8a88e
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-08-31 16:43:57 +0800

    fx

--- src/main/java/com/jiduauto/monilog/GrpcMoniLogInterceptor.java
+++ src/main/java/com/jiduauto/monilog/GrpcMoniLogInterceptor.java
@@ -151,6 +151,7 @@ class GrpcMoniLogInterceptor {
                     params.setSuccess(false);
                     params.setMsgInfo(status.getDescription());
                     params.setMsgCode(status.getCode().name());
+                    params.setException(status.asException());
                 }
                 super.onClose(status, trailers);
                 MoniLogUtil.log(params);
