ccf862960b3d7e5dbcd5669d63c0c6c6fa21891e
pei.ye01 <pei.ye01@jiduauto.com>
2023-09-22 16:45:12 +0800

    Merge branch 'feature/opt_109_dev_snapshot' into feature/opt_109_snapshot
    # Conflicts:
    #	src/main/java/com/jiduauto/monilog/MonilogEnhancer.java

--- src/main/java/com/jiduauto/monilog/ResultParser.java
+++ src/main/java/com/jiduauto/monilog/ResultParser.java
@@ -180,13 +180,10 @@ final class ResultParser {
         boolean isMethod = path.contains("(") && path.contains(")");
         if (!isMethod) {
             try {
-                Object val = JSONPath.eval(obj, path);
+                boolean isEmptyString = obj instanceof CharSequence && ((CharSequence) obj).length() == 0;
+                Object val = isEmptyString ? null : JSONPath.eval(obj, path);
                 boolean foundPath = val != null || JSONPath.contains(obj, path);
                 return new TempResult(val, foundPath);
-            } catch (NullPointerException e){
-                MoniLogUtil.innerDebug("npe");
-                // 在尝试解析返回编码时会逐个匹配，找不到会报错 :$.xx NullPointerException 如$.subResultCode NullPointerException
-                // 此错误属于正常情况，直接忽略
             } catch (Throwable e) {
                 MoniLogUtil.innerDebug("resultParser evalVal error, obj:{}, path:{}", JSON.toJSONString(obj), path, e);
             }
