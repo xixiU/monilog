edcfa147717f6af724d1355012a6c6849ae8b3ae
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-10-16 12:22:39 +0800

    fx send add comment

--- src/main/java/com/jiduauto/monilog/RocketMqMoniLogInterceptor.java
+++ src/main/java/com/jiduauto/monilog/RocketMqMoniLogInterceptor.java
@@ -127,6 +127,7 @@ public final class RocketMqMoniLogInterceptor {
 
         @Override
         public void sendMessageAfter(SendMessageContext context) {
+            // 异步的时候会调用两次sendMessageAfter,第二次的sendResult会有发送结果，取第二次
             if (CommunicationMode.ASYNC == context.getCommunicationMode() && context.getSendResult() == null) {
                 return;
             }
