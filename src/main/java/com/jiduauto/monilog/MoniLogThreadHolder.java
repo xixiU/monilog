b68602dd414b54cc4b0f4f4b1c59d65cdc6e2280
pei.ye01 <pei.ye01@jiduauto.com>
2023-11-07 12:41:29 +0800

    Merge branch 'feature/opt_114_snapshot' of jidudev.com:vehicle/sale-support/monilog into feature/opt_114_snapshot
    # Conflicts:
    #	src/main/java/com/jiduauto/monilog/DefaultMoniLogPrinter.java

--- src/main/java/com/jiduauto/monilog/MoniLogThreadHolder.java
+++ src/main/java/com/jiduauto/monilog/MoniLogThreadHolder.java
@@ -12,7 +12,10 @@ public class MoniLogThreadHolder {
      */
     private static final ThreadLocal<String> TRACE_ID_THREAD_LOCAL = new ThreadLocal<>();
 
-
+    /**
+     *
+     * 设置自定义traceId，注意，一般情况下不需要设置自定义traceId，Monilog会自动从MDC中提取traceId。 除非业务方法内部自己操作(添加并删除)了traceId导致Monilog拦截器在业务方法前后均不能从MDC中取到有效traceId时，可以由业务方借助此方法将自定义的traceId告知Monilog
+     */
     public static void setTraceId(String traceId) {
         TRACE_ID_THREAD_LOCAL.set(traceId);
     }
