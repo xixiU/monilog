194f84ade9b39727353b3e9f35ae053fe08cc098
rongjie.yuan <rongjie.yuan@jiduauto.com>
2023-11-07 13:31:13 +0800

    fx

--- src/main/java/com/jiduauto/monilog/MoniLogThreadHolder.java
+++ src/main/java/com/jiduauto/monilog/MoniLogThreadHolder.java
@@ -14,7 +14,7 @@ public class MoniLogThreadHolder {
 
     /**
      *
-     * 设置自定义traceId，注意，一般情况下不需要设置自定义traceId，Monilog会自动从MDC中提取traceId。 除非业务方法内部自己操作(添加并删除)了traceId导致Monilog拦截器在业务方法前后均不能从MDC中取到有效traceId时，可以由业务方借助此方法将自定义的traceId告知Monilog
+     * 设置自定义traceId，注意，一般情况下不需要设置自定义traceId，MoniLog会自动从MDC中提取traceId。 除非业务方法内部自己操作(添加并删除)了traceId导致MoniLog拦截器在业务方法前后均不能从MDC中取到有效traceId时，可以由业务方借助此方法将自定义的traceId告知MoniLog
      */
     public static void setTraceId(String traceId) {
         TRACE_ID_THREAD_LOCAL.set(traceId);
