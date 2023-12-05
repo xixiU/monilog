dd013e1dc11609b49dc55c0d125ec6705e107ac4
pei.ye01 <pei.ye01@jiduauto.com>
2023-12-05 16:06:51 +0800

    stream判断

--- src/main/java/com/jiduauto/monilog/SpringUtils.java
+++ src/main/java/com/jiduauto/monilog/SpringUtils.java
@@ -56,7 +56,7 @@ class SpringUtils implements BeanFactoryPostProcessor, ApplicationContextAware {
      */
     public static String LOG_PREFIX = "monilog_";
 
-    private static final Map<Class<?>, Object> INSTANCE_MAP = new ConcurrentHashMap<>();
+    private static final Map<Class<?>, Object> INSTANCE_MAP = new ConcurrentHashMap<>(10);
 
     @SuppressWarnings("NullableProblems")
     @Override
