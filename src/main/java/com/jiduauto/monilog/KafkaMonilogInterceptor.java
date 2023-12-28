bb6cfd959524fff5fd8a7dca5d78bd26d019d70a
pei.ye01 <pei.ye01@jiduauto.com>
2023-12-28 17:22:04 +0800

    add kafka

--- src/main/java/com/jiduauto/monilog/KafkaMonilogInterceptor.java
+++ src/main/java/com/jiduauto/monilog/KafkaMonilogInterceptor.java
@@ -22,6 +22,7 @@ public final class KafkaMonilogInterceptor {
     public static <K, V> ConsumerInterceptor<K, V> getConsumerInterceptor() {
         return new KafkaMonilogInterceptor.MonilogConsumerInterceptor<>();
     }
+
     public static <K, V> ProducerInterceptor<K, V> getProducerInterceptor() {
         return new KafkaMonilogInterceptor.MonilogProducerInterceptor<>();
     }
@@ -31,33 +32,49 @@ public final class KafkaMonilogInterceptor {
 
         @Override
         public ConsumerRecords<K, V> onConsume(ConsumerRecords<K, V> records) {
+            // 判断开关
+            if (!ComponentEnum.kafka_consumer.isEnable()) {
+                return records;
+            }
+            log.warn("monilog kafka onConsume...");
             return records;
         }
 
         @Override
-        public void onCommit(Map<TopicPartition, OffsetAndMetadata> offsets) {}
+        public void onCommit(Map<TopicPartition, OffsetAndMetadata> offsets) {
+        }
 
         @Override
-        public void close() {}
+        public void close() {
+        }
 
         @Override
-        public void configure(Map<String, ?> configs) {}
+        public void configure(Map<String, ?> configs) {
+        }
     }
 
     private static class MonilogProducerInterceptor<K, V> implements ProducerInterceptor<K, V> {
 
         @Override
         public ProducerRecord<K, V> onSend(ProducerRecord<K, V> record) {
+            // 判断开关
+            if (!ComponentEnum.kafka_producer.isEnable()) {
+                return record;
+            }
+            log.warn("monilog kafka onSend...");
             return record;
         }
 
         @Override
-        public void onAcknowledgement(RecordMetadata metadata, Exception exception) {}
+        public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+        }
 
         @Override
-        public void close() {}
+        public void close() {
+        }
 
         @Override
-        public void configure(Map<String, ?> configs) {}
+        public void configure(Map<String, ?> configs) {
+        }
     }
 }
